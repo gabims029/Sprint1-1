39fd1b6c3a1704b70f8ebf4557360ce850bef7c5
LeoPedroso2323 <leonardopedroso857@gmail.com>
2024-12-02 08:32:21 -0300

    Atualização erros delete

--- API_Sprint/src/controllers/controllerReserva.js
+++ API_Sprint/src/controllers/controllerReserva.js
@@ -175,6 +175,7 @@ module.exports = class controllerReserva {
         if (results.affectedRows === 0) {
           return res.status(404).json({ error: "Reserva não encontrada" });
         }
+        return res.status(200).json({ message:"Reserva excluída com sucesso" });
       });
     } catch (error) {
       console.log("Erro ao executar a consulta", error);
